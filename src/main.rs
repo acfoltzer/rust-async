cbd3fe757c0d2dd6ff644ae15db68dd3594df8a8
Frederik Deweerdt <fdeweerdt@fastly.com>
2020-04-22 17:42:32 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use lazy_static::lazy_static;
-use std::net::SocketAddr;
-use std::error::Error;
-use std::net::ToSocketAddrs;
-use tokio::io::{AsyncRead, AsyncWrite};
+use futures::future::TryFutureExt;
+use futures::task::{self, Poll};
+use futures::{self, Future};
 use hyper::client::connect::{Connected, Connection};
+use hyper::{Body, Client, Uri};
+use lazy_static::lazy_static;
+use std::error::Error;
+use std::io;
+use std::net::SocketAddr;
+use std::net::ToSocketAddrs;
+use std::pin::Pin;
+use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::net::TcpStream;
 use tokio_tls;
-use hyper::{Client,Uri,Body};
 use tokio_tls::TlsStream;
-use std::pin::Pin;
-use futures::future::TryFutureExt;
-use futures::{self, Future};
-use futures::task::{self, Poll};
-use std::io;
-
 
 pub enum UpstreamConnectorTransport {
     Tls(TlsStream<TcpStream>),
@@ -69,7 +68,6 @@ impl Connection for UpstreamConnectorTransport {
     }
 }
 
-
 #[derive(Debug, Clone)]
 pub struct UpstreamTlsConnector<'a> {
     pub socket_addr: &'a SocketAddr,
@@ -86,15 +84,15 @@ impl<'a> UpstreamTlsConnector<'a> {
 }
 
 lazy_static! {
-        static ref TLS_CONNECTOR: tokio_tls::TlsConnector =
-                    tokio_tls::TlsConnector::from(native_tls::TlsConnector::builder().build().unwrap());
+    static ref TLS_CONNECTOR: tokio_tls::TlsConnector =
+        tokio_tls::TlsConnector::from(native_tls::TlsConnector::builder().build().unwrap());
 }
 
 impl<'a> hyper::service::Service<hyper::Uri> for UpstreamTlsConnector<'a> {
     type Response = TlsStream<TcpStream>;
     type Error = std::io::Error;
 
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'a >>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'a>>;
 
     fn poll_ready(&mut self, _: &mut task::Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
@@ -108,51 +106,48 @@ impl<'a> hyper::service::Service<hyper::Uri> for UpstreamTlsConnector<'a> {
                     static DOMAIN: &str = "example.org";
                     self.tls_cx
                         /*
-   Compiling tls v0.1.0 (/home/def/p/proglets/minimal-rust/tls)
-error[E0495]: cannot infer an appropriate lifetime for autoref due to conflicting requirements
-   --> src/main.rs:111:26
-    |
-111 |                         .connect(&DOMAIN, stream)
-    |                          ^^^^^^^
-    |
-note: first, the lifetime cannot outlive the anonymous lifetime #1 defined on the method body at 103:5...
-   --> src/main.rs:103:5
-    |
-103 | /     fn call(&mut self, _uri: Uri) -> Self::Future {
-104 | |         Box::pin(
-105 | |             TcpStream::connect(self.socket_addr)
-106 | |                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-...   |
-114 | |         )
-115 | |     }
-    | |_____^
-note: ...so that reference does not outlive borrowed content
-   --> src/main.rs:109:21
-    |
-109 |                     self.tls_cx
-    |                     ^^^^^^^^^^^
-    = note: but, the lifetime must be valid for the static lifetime...
-note: ...so that the expression is assignable
-   --> src/main.rs:104:9
-    |
-104 | /         Box::pin(
-105 | |             TcpStream::connect(self.socket_addr)
-106 | |                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
-107 | |                 .and_then(move |stream| {
-...   |
-113 | |                 }),
-114 | |         )
-    | |_________^
-    = note: expected  `std::pin::Pin<std::boxed::Box<(dyn core::future::future::Future<Output = std::result::Result<tokio_tls::TlsStream<tokio::net::tcp::stream::TcpStream>, std::io::Error>> + std::marker::Send + 'static)>>`
-               found  `std::pin::Pin<std::boxed::Box<dyn core::future::future::Future<Output = std::result::Result<tokio_tls::TlsStream<tokio::net::tcp::stream::TcpStream>, std::io::Error>> + std::marker::Send>>`
-
-error: aborting due to previous error
-
-For more information about this error, try `rustc --explain E0495`.
-error: could not compile `tls`.
-
-To learn more, run the command again with --verbose.
-                        */
+                           Compiling tls v0.1.0 (/home/def/p/proglets/minimal-rust/tls)
+                        error[E0495]: cannot infer an appropriate lifetime for autoref due to conflicting requirements
+                           --> src/main.rs:111:26
+                            |
+                        111 |                         .connect(&DOMAIN, stream)
+                            |                          ^^^^^^^
+                            |
+                        note: first, the lifetime cannot outlive the anonymous lifetime #1 defined on the method body at 103:5...
+                           --> src/main.rs:103:5
+                            |
+                        103 | /     fn call(&mut self, _uri: Uri) -> Self::Future {
+                        104 | |         Box::pin(
+                        105 | |             TcpStream::connect(self.socket_addr)
+                        106 | |                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+                        ...   |
+                        114 | |         )
+                        115 | |     }
+                            | |_____^
+                        note: ...so that reference does not outlive borrowed content
+                           --> src/main.rs:109:21
+                            |
+                        109 |                     self.tls_cx
+                            |                     ^^^^^^^^^^^
+                            = note: but, the lifetime must be valid for the static lifetime...
+                        note: ...so that the expression is assignable
+                           --> src/main.rs:104:9
+                            |
+                        104 | /         Box::pin(
+                        105 | |             TcpStream::connect(self.socket_addr)
+                        106 | |                 .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+                        107 | |                 .and_then(move |stream| {
+                        ...   |
+                        113 | |                 }),
+                        114 | |         )
+                            | |_________^
+                            = note: expected  `std::pin::Pin<std::boxed::Box<(dyn core::future::future::Future<Output = std::result::Result<tokio_tls::TlsStream<tokio::net::tcp::stream::TcpStream>, std::io::Error>> + std::marker::Send + 'static)>>`
+                                       found  `std::pin::Pin<std::boxed::Box<dyn core::future::future::Future<Output = std::result::Result<tokio_tls::TlsStream<tokio::net::tcp::stream::TcpStream>, std::io::Error>> + std::marker::Send>>`
+                        error: aborting due to previous error
+                        For more information about this error, try `rustc --explain E0495`.
+                        error: could not compile `tls`.
+                        To learn more, run the command again with --verbose.
+                                                */
                         .connect(&DOMAIN, stream)
                         .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
                 }),
@@ -181,12 +176,11 @@ impl<'a> Clone for UpstreamConnector<'a> {
     }
 }
 
-
 impl<'a> hyper::service::Service<Uri> for UpstreamConnector<'a> {
     type Response = UpstreamConnectorTransport;
     type Error = std::io::Error;
 
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'a >>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'a>>;
 
     fn poll_ready(&mut self, context: &mut task::Context<'_>) -> Poll<Result<(), Self::Error>> {
         match self {
@@ -217,7 +211,9 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let connector = UpstreamConnector::Tls(UpstreamTlsConnector::new(&addr, &tls_cx));
     let client: Client<_, Body> = Client::builder().build(connector.clone());
 
-    let res = client.get(Uri::from_static("https://httpbin.org/ip")).await?;
+    let res = client
+        .get(Uri::from_static("https://httpbin.org/ip"))
+        .await?;
     println!("status: {}", res.status());
     let buf = hyper::body::to_bytes(res).await?;
     println!("body: {:?}", buf);
